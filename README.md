6131e226332ddcbfeeca1900fe01c3c4417152c8
Kazuyuki Kohno <kkohno@twitter.com>
2013-04-12 14:54:13 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+SublimeSnippetMaker
+========
+
+Sublime Text 2 plugin that provides you to make snippets easily.
+
+## Installation
+
+```
+$ cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
+$ git clone https://github.com/jugyo/SublimeSnippetMaker.git SnippetMaker
+```
+
+## Usage
+
+Execute the 'Make Snippet' command in the Command Palette.
---
+++ snippet_maker.py
@@ -0,0 +1,41 @@
+import sublime, sublime_plugin
+import os
+
+template = """<snippet>
+  <content><![CDATA[
+%s
+]]></content>
+  <!-- Optional: Set a tabTrigger to define how to trigger the snippet -->
+  <tabTrigger>%s</tabTrigger>
+  <!-- Optional: Set a scope to limit where the snippet will trigger -->
+  <scope>%s</scope>
+</snippet>"""
+
+class MakeSnippetCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.scope = self.view.scope_name(self.view.sel()[0].begin()).split(' ')[0]
+        self.snippet_text = "\n".join([self.view.substr(i) for i in self.view.sel()])
+
+        self.view.window().show_input_panel('Trigger', '', self.set_trigger, None, None)
+
+    def set_trigger(self, trigger):
+        self.trigger = trigger
+        self.view.window().show_input_panel('File Name', 'Default.sublime-snippet', self.make_snippet, None, None)
+
+    def make_snippet(self, file_name):
+        if len(file_name) > 0:
+            file_path = os.path.join(sublime.packages_path(), 'User', file_name)
+
+            if os.path.exists(file_path):
+                if sublime.ok_cancel_dialog('Override %s?' % file_name) == False:
+                    return
+
+            file = open(file_path, "wb")
+            snippet_xml = template % (self.snippet_text, self.trigger, self.scope)
+            file.write(snippet_xml)
+            file.close()
+
+            self.view.window().open_file(file_path)
+
+        else:
+            sublime.error_message('Please specify the snippet name!!')

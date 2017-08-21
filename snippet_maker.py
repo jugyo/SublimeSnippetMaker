199bb8a2b98478f01636081b88f5b713d5bcf3cd
Koen Lageveen <koen.lageveen@me.com>
2017-08-21 12:58:22 +0200

    lint

--- snippet_maker.py
+++ snippet_maker.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 import sublime
 import sublime_plugin
@@ -15,39 +16,74 @@ template = """<snippet>
 
 class MakeSnippetCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.snippet_text = "\n".join([self.view.substr(i) for i in self.view.sel()])
-        self.view.window().show_input_panel('Trigger', '', self.set_trigger, None, None)
+        self.snippet_text = "\n".join(
+            [self.view.substr(i) for i in self.view.sel()]
+        )
+        self.view.window().show_input_panel(
+            'Trigger',
+            '',
+            self.set_trigger,
+            None,
+            None
+        )
 
     def set_trigger(self, trigger):
         self.trigger = trigger
-        self.view.window().show_input_panel('Description', '', self.set_description, None, None)
+        self.view.window().show_input_panel(
+            'Description',
+            '',
+            self.set_description,
+            None,
+            None
+        )
 
     def set_description(self, description):
         self.description = description
-        scopes = self.view.scope_name(self.view.sel()[0].begin()).strip().replace(' ', ', ')
-        self.view.window().show_input_panel('Scope', scopes, self.set_scopes, None, None)
+        scopes = self.view.scope_name(
+            self.view.sel()[0].begin()
+        ).strip().replace(' ', ', ')
+        self.view.window().show_input_panel(
+            'Scope',
+            scopes,
+            self.set_scopes,
+            None,
+            None
+        )
 
     def set_scopes(self, scopes):
         self.scopes = scopes
         self.ask_file_name()
 
     def ask_file_name(self):
-        self.view.window().show_input_panel('File Name', self.trigger + '.sublime-snippet', self.make_snippet, None, None)
+        self.view.window().show_input_panel(
+            'File Name',
+            self.trigger + '.sublime-snippet',
+            self.make_snippet,
+            None,
+            None
+        )
 
     def make_snippet(self, file_name):
         settings = sublime.load_settings('Breadcrumbs.sublime-settings')
         location = settings.get('snippet_location', 'SnippetMaker')
 
-        file_path = os.path.join(sublime.packages_path(), 'User', location, file_name)
+        file_path = os.path.join(
+            sublime.packages_path(),
+            'User',
+            location,
+            file_name
+        )
 
-        if os.path.exists(file_path) and not sublime.ok_cancel_dialog('Override %s?' % file_name):
+        if os.path.exists(file_path) and not sublime.ok_cancel_dialog(
+            'Override %s?' % file_name
+        ):
             self.ask_file_name()
             return
 
         try:
             self.write_snippet(file_path)
         except OSError:
-            sublime.error_message('Please specify a valid snippet file name! i.e. `awesome.sublime-snippet`')
+            sublime.error_message('Please specify a valid file name, i.e. `awesome.sublime-snippet`')  # noqa: E501
             self.ask_file_name()
         else:
             self.view.window().open_file(file_path)
